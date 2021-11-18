39205271aeb34a663aedeaa6dccdefb9f49932c9
pmenyangbo <pmenyangbo.f18@warren-wilson.edu>
2021-11-18 16:09:16 -0500

    Better version almost done!

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import folium
-CONSTANT = "html/map/file/Data.html"
-if __name__ == '__main__':
-    map1 = folium.Map(location=(35.61, -82.44), zoom_start=50)
-    map1.save('Data/map1.html')
+import csv
 
+#import folium
+CONSTANT = "html/map/file/Data.html"
+
+    # map1 = folium.Map(location=(35.61, -82.44), zoom_start=50)
+    #map1.save('Data/map1.html')
+if __name__ == '__main__':
+    fv = open("data/volcanoes.tsv", "r")
+    dialect= csv.Sniffer().sniff(fv.read())
+    fv.seek(0)
+    reader = csv.DictReader(fv, dialect=dialect)
+    rows = []
+    for row in reader:
+        rows.append(row)
+    print("dude")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
